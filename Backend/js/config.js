3dab2d6fc9dfbe979cb64c6e4dc97da80397b51e
Nacho Viano Martinez <nviano@hotmail.es>
2018-10-02 19:19:07 +0200

    sin password

--- Backend/js/config.js
+++ Backend/js/config.js
@@ -3,11 +3,11 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "viano1984",
+    password: "",
     database: "load_control"
 });
 
-con.connect(function(err) {
+con.connect(function (err) {
     if (err) throw err;
     console.log("Conectado! Dale ahi compadre!");
 
